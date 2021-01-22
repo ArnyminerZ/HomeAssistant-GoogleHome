1f7a94aa90b509bb6253c71ccaf418aa4e924321
Arnau Mora <arnyminer.z@gmail.com>
2021-01-22 17:41:43 +0100

    :sparkles: Added device searcher and data loader

--- get_alarms.py
+++ get_alarms.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from get_tokens import master_token, access_token
-from load_params import device_ip, device_name
+from load_params import device_ip, device_name, fetch_path
 
 if master_token is None:
     print("Master token not found")
@@ -27,11 +27,22 @@ script_dir_path = script_path.parent
 lat_result = os.popen(f"{script_dir_path}/grpcurl -H 'authorization: Bearer {access_token}' -import-path {script_dir_path} -proto {script_dir_path}/google/internal/home/foyer/v1.proto googlehomefoyer-pa.googleapis.com:443 google.internal.home.foyer.v1.StructuresService/GetHomeGraph | jq '.home.devices[] | {{deviceName, localAuthToken}}'")
 lat_data = lat_result.read()[:-1] # -1 for removing the last line jump
 lat_data = "[" + lat_data.replace("}", "},")[:-1] + "]" # Format the JSON correctly
-lat_items = []
 
-# Now, remove the items that doesn't provide a valid local auth token
+found_device = False
 for element in json.loads(lat_data):
-    if element["localAuthToken"] is not None:
-        lat_items.append(element)
+    token = element["localAuthToken"]
+    name = element["deviceName"]
+    if token is None:
+        continue
+    if name == device_name:
+        found_device = True
+        get_request = requests.get(
+            f"https://{device_ip}:8443/setup/{fetch_path}",
+            headers={'cast-local-authorization-token': token},
+            verify=False,
+        )
+        request_json = get_request.json()
+        print(request_json)
 
-print(lat_items)
+if not found_device:
+    print("Error: The specified device was not found. Check your parameters.")
