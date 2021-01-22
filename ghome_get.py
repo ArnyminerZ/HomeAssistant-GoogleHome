2312b3d5ff6ea238c02c4065ee470e1058377c8e
ArnyminerZ <Arnyminer.Z@gmail.com>
2021-01-22 19:05:52 +0100

    :twisted_rightwards_arrows: Implemented mqtt (#2)
    * :memo: Added output MQTT
    
    * :sparkles: Added output parameter
    
    * :bug: Fixed a typo
    
    * :memo: Added connection note
    
    * :memo: Added connection note
    
    * :sparkles: Implemented MQTT
    
    * :sparkles: Finished implementing MQTT

--- ghome_get.py
+++ ghome_get.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
-import os
-import sys, getopt
+import os, sys, random
 import json
 import requests
 from pathlib import Path
+import time
 
 from dotenv import load_dotenv
 load_dotenv()
 
+from paho.mqtt import client as mqtt_client
+
 from get_tokens import master_token, access_token
-from load_params import device_ip, device_name, fetch_path
+from load_params import device_ip, device_name, fetch_path, output_param
 
 if master_token is None:
     print("Master token not found")
@@ -20,8 +22,24 @@ if access_token is None:
     sys.exit(1)
 
 if device_name is None or device_ip is None or fetch_path is None:
-    print("ghome_get.py -i <device-ip> -n <device-name> -p <path>")
+    print("ghome_get.py -i <device-ip> -n <device-name> -p <path> -o [output]")
     sys.exit(1)
+
+def connect_mqtt(broker, port, username, password, client_id, topic, contents):
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+            client.publish(topic, contents)
+        else:
+            print(f"Failed to connect, return code {rc}")
+            sys.exit(1)
+
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    if username is not None:
+        client.username_pw_set(username, password)
+    client.connect(broker, port)
+    return client
 
 script_path = Path(os.path.realpath(__file__))
 script_dir_path = script_path.parent
@@ -39,12 +57,49 @@ for element in json.loads(lat_data):
     if name == device_name:
         found_device = True
         get_request = requests.get(
-            f"https://{device_ip}:8443/setup/{fetch_path}",
+            f"https://{device_ip}:8443/setup{fetch_path}",
             headers={'cast-local-authorization-token': token},
             verify=False,
         )
         request_json = get_request.text
         print(request_json)
+
+        # Perform all the outputs
+        if output_param is not None:
+            if output_param.startswith("mqtt://"):
+                print("Publishing through MQTT...")
+                # Get without mqtt://
+                req = output_param[7:]
+                # Split from topic
+                splt = req.split("/")
+                # Get first half
+                where = splt[0]
+                # Get topic
+                topic = splt[1]
+                # Divide auth and host
+                splt = where.split("@")
+                # Get auth and host
+                auth = splt[0]
+                host = splt[1]
+                # Get username and password
+                splt = auth.split(":") if len(auth) > 0 else None
+                username = splt[0] if splt is not None else None
+                password = splt[1] if splt is not None else None
+                # Get address and port
+                splt = host.split(":")
+                address = splt[0]
+                port = int(splt[1])
+
+                if username is not None:
+                    print(f"  Authentication required (username:{username},pass={password}).")
+
+                client_id = "ghome_" + str(random.randint(0, 1000))
+                client = connect_mqtt(address, port, username, password, client_id, topic, request_json)
+                client.loop_start()
+                time.sleep(2)
+            else:
+                print("Found an output parameter, but the contents are not valid.")
+                print("Please check README for orientation on how to run the command: https://github.com/ArnyminerZ/HomeAssistant-GoogleHome#running")
 
 if not found_device:
     print("Error: The specified device was not found. Check your parameters.")
--- load_params.py
+++ load_params.py
@@ -5,14 +5,15 @@ import sys, getopt
 device_ip = None
 device_name = None
 fetch_path = None
+output_param = None
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"hi:n:p:",["ip=","name=","path="])
+    opts, args = getopt.getopt(sys.argv[1:],"hi:n:p:o:",["ip=","name=","path=","output="])
 except getopt.GetoptError:
-    print('ghome_get.py -i <device-ip> -n <device-name> -p <path>')
+    print('ghome_get.py -i <device-ip> -n <device-name> -p <path> -o [output]')
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-h':
-        print ('ghome_get.py -i <device-ip> -n <device-name> -p <path>')
+        print ('ghome_get.py -i <device-ip> -n <device-name> -p <path> -o [output]')
         sys.exit()
     elif opt in ("-i", "--ip"):
         device_ip = arg
@@ -20,3 +21,5 @@ for opt, arg in opts:
         device_name = arg
     elif opt in ("-p", "--path"):
         fetch_path = arg
+    elif opt in ("-o", "--output"):
+        output_param = arg
