a2e21688be5e4d34daa8c5cd2e101b5cba910273
Arnau Mora <arnyminer.z@gmail.com>
2021-01-22 17:41:18 +0100

    :sparkles: Added path parameter

--- load_params.py
+++ load_params.py
@@ -4,16 +4,19 @@ import sys, getopt
 
 device_ip = None
 device_name = None
+fetch_path = None
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"hi:n:",["ip=","name="])
+    opts, args = getopt.getopt(sys.argv[1:],"hi:n:p:",["ip=","name=","path="])
 except getopt.GetoptError:
-    print('get_alarms.py -i <device-ip> -n <device-name>')
+    print('get_alarms.py -i <device-ip> -n <device-name> -p <path>')
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-h':
-        print ('get_alarms.py -i <device-ip> -n <device-name>')
+        print ('get_alarms.py -i <device-ip> -n <device-name> -p <path>')
         sys.exit()
     elif opt in ("-i", "--ip"):
         device_ip = arg
     elif opt in ("-n", "--name"):
         device_name = arg
+    elif opt in ("-p", "--path"):
+        fetch_path = arg
